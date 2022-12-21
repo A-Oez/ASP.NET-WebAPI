2710d8e8a083145a9140f80fc0cb9e83451eeb41
A-Oez <60219737+A-Oez@users.noreply.github.com>
2022-12-21 20:55:23 +0100

    Update ArticleService.cs

--- IntroductionASPNET/Services/ArticleService/ArticleService.cs
+++ IntroductionASPNET/Services/ArticleService/ArticleService.cs
@@ -162,7 +162,7 @@
         {
             if (checkDB is null)
             {
-                var exception = new NullReferenceException("the given ID " + checkDB.Articlenumber.ToString() + " does not exists");
+                var exception = new NullReferenceException("the given ID does not exists");
                 return new Result<Article>(exception);
             }
 
